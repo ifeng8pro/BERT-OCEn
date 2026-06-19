a51f38cb2f0663349b909d05821b1f6b72e16fe1
ifeng8pro <fengcx1227@163.com>
2026-06-19 18:43:59 +0800

    Update finetune_bert.py

--- src/fine_tune_bert/finetune_bert.py
+++ src/fine_tune_bert/finetune_bert.py
@@ -170,7 +170,7 @@ def real_training(model, train_texts, train_labels, tokenizer, batch_size=32, ma
     warmup_start = time.time()
     # Training loop
     print(f"begin training，there are {epochs} epoch...")
-    print("!The first batch loading is time-consuming.，waiting！")
+    print("!The first batch loading is time-consuming. waiting！")
     # Start loop training model (main fine-tuning process)
     for epoch in range(epochs): #epochs=4
         model.train()
