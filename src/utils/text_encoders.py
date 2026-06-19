5a3998143c1bd9934d034b5ab542b8290f4310e5
ifeng8pro <fengcx1227@163.com>
2026-06-19 18:36:51 +0800

    Update text_encoders.py

--- src/utils/text_encoders.py
+++ src/utils/text_encoders.py
@@ -11,7 +11,7 @@ class MyBertTokenizer(BertTokenizer):
     def __init__(self, vocab_file, do_lower_case=True, append_eos=False, add_special_tokens=True):
         super().__init__(vocab_file, do_lower_case=do_lower_case)
         self.append_eos = append_eos
-        self.add_special_tokens = add_special_tokens  # 新增参数，控制是否添加特殊token
+        self.add_special_tokens = add_special_tokens  # Add new parameter to control special token addition
 
         self.itos = list(self.vocab.keys())
         self.stoi = {token: index for index, token in enumerate(self.itos)}
@@ -24,14 +24,14 @@ class MyBertTokenizer(BertTokenizer):
         text = self.tokenize(text)
         unknown_index = self.stoi['[UNK]']  # overwrite unknown_index according to BertTokenizer vocab
 
-        # 添加BERT特殊token
+        # Add BERT special tokens
         if self.add_special_tokens:
             # [CLS] + tokens + [SEP]
-            vector = [self.stoi['[CLS]']]  # CLS token在开头
+            vector = [self.stoi['[CLS]']]  # CLS token at the beginning
             vector.extend([self.stoi.get(token, unknown_index) for token in text])
-            vector.append(self.stoi['[SEP]'])  # SEP token在结尾
+            vector.append(self.stoi['[SEP]'])  # SEP token at the end
         else:
-            # 原始逻辑（不添加特殊token）
+            # Original logic (no special tokens added)
             vector = [self.stoi.get(token, unknown_index) for token in text]
 
         if self.append_eos:
